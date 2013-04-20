56913a806bdcbb911cff1348bb001b0af219a08b
enrcmoo1 <C.Moore1@2010.ljmu.ac.uk>
2013-04-20 16:30:47 +0100

    second commit
    20th April

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,34 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def holidays
+  end
+
+  def sessions
+  end
+
+  def racing
+  end
+
+  def social
+  end
+
+  def contact
+  end
+
+  def faq
+  end
+
+  def events
+  end
+
+  def login
+  end
+
+  def register
+  end
+end
